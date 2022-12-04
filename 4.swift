5f34bcb7b8250e3603cab6b08e80579940efe392
pin2t <pin2t@mail.ru>
2022-12-04 08:57:21 +0300

    simplify

--- 4.swift
+++ 4.swift
@@ -1,9 +1,9 @@
 import Foundation
 var n1 = 0, n2 = 0
 while let line = readLine() {
-    let pairs = line.components(separatedBy: ["-", ","])
-    if Int(pairs[0])! <= Int(pairs[2])! && Int(pairs[1])! >= Int(pairs[3])! ||
-       Int(pairs[2])! <= Int(pairs[0])! && Int(pairs[3])! >= Int(pairs[1])! { n1 += 1 }
-    if Int(pairs[1])! >= Int(pairs[2])! && Int(pairs[3])! >= Int(pairs[0])! { n2 += 1 }  
+    let pairs = line.components(separatedBy: ["-", ","]).map { Int($0)! }
+    if pairs[0] <= pairs[2] && pairs[1] >= pairs[3] ||
+       pairs[2] <= pairs[0] && pairs[3] >= pairs[1] { n1 += 1 }
+    if pairs[1] >= pairs[2] && pairs[3] >= pairs[0] { n2 += 1 }  
 }
 print(n1, n2)
