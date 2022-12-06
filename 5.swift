0dd774e6d3b6da69778bfb84d6d74b2b34704000
pin2t <pin2t@mail.ru>
2022-12-06 08:57:02 +0300

    day 6

---
+++ 5.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+extension String {
+    var numbers: [Int] {
+        guard let regex = try? NSRegularExpression(pattern: "\\d+", options: [.caseInsensitive]) else { return [] }
+        let matches  = regex.matches(in: self, options: [], range: NSMakeRange(0, self.count))
+        return matches.map { match in
+            String(self[Range(match.range, in: self)!])
+        }.map { Int($0)! }
+    }
+}
+
+class Stack {
+    var crates: [Character] = []
+
+    func move(to: Stack, n: Int) {
+        
+    }
+}
+
+var lines: [String] = []
+while let line = readLine() {
+    if line == "" { break }
+    lines.append(line)
+}
+print(lines)
+while let line = readLine() {
+    print(line.numbers)
+}
---
+++ kkk/kkk/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  kkk
+//
+//  Created by Ilya Pokolev on 06.12.2022.
+//
+
+import Foundation
+
+print("Hello, World!")
+
