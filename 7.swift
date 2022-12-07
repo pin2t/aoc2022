c26ff0c7904418216c419a89a040a1ec3f03d600
pin2t <pin2t@mail.ru>
2022-12-07 21:11:42 +0300

    more oo and functional

--- 7.swift
+++ 7.swift
@@ -7,7 +7,14 @@ class Directory {
     var size: Int {
         return files.values.reduce(0, +) + subDirs.values.map { $0.size }.reduce(0, +)
     }
+    var allSizes: [Int] {
+        var sizes: [Int] = []
+        self.forEach { sizes.append($0.size) }
+        return sizes
+    }
 
+    init() { }
+    init(_ p: Directory) { parent = p }
     func forEach(_ f: (Directory) -> Void) {
         f(self)
         subDirs.forEach { $0.value.forEach(f) }
@@ -18,29 +25,19 @@ var root = Directory()
 var current = root
 while let line = readLine() {
     let words = line.components(separatedBy: [" "])
-    if words[0] == "$" && words[1] == "ls" { 
-        continue 
-    } else if words[0] == "$" && words[1] == "cd" {
-        switch words[2] {
-            case "/":  current = root
-            case "..": current = current.parent!
-            default:   current = current.subDirs[words[2]]!
+    if words[0] == "$" { 
+        if words[1] == "cd" {
+            switch words[2] {
+                case "/":  current = root
+                case "..": current = current.parent!
+                default:   current = current.subDirs[words[2]]!
+            }
         }
     } else if words[0] == "dir" {
-        let dir = Directory()
-        dir.parent = current
-        current.subDirs[words[1]] = dir
+        current.subDirs[words[1]] = Directory(current)
     } else {
         current.files[words[1]] = Int(words[0])!
     }
 }
-var total = 0, smallest = 10000000000, rootSize = root.size
-root.forEach {
-    if $0.size <= 100000 { 
-        total += $0.size 
-    }
-    if ($0.size >= 30000000-(70000000-rootSize)) && $0.size < smallest { 
-        smallest = $0.size 
-    }
-}
-print(total, smallest)
+print(root.allSizes.filter {$0 <= 100000}.reduce(0, +), 
+      root.allSizes.filter {$0 >= 30000000-(70000000-root.size)}.min() ?? 0)
