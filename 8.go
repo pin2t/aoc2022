6498c27ae4a316c261fc1a61206cc7c047694f65
pin2t <pin2t@mail.ru>
2022-12-08 09:46:36 +0300

    combine two parts

--- 8.go
+++ 8.go
@@ -13,68 +13,31 @@ func main() {
 		grid = append(grid, scanner.Text())
 	}
 	n := len(grid[0])*2 + len(grid)*2 - 4 // tress on edges
-	for i := 1; i < len(grid[0])-1; i++ {
-		for j := 1; j < len(grid)-1; j++ {
-			lower := true
-			for k := 0; k < i && lower; k++ {
-				if grid[j][k] >= grid[j][i] {
-					lower = false
-				}
-			}
-			if lower {
-				n += 1
-				continue
-			}
-			lower = true
-			for k := i + 1; k < len(grid[0]) && lower; k++ {
-				if grid[j][k] >= grid[j][i] {
-					lower = false
-				}
-			}
-			if lower {
-				n += 1
-				continue
-			}
-			lower = true
-			for k := 0; k < j && lower; k++ {
-				if grid[k][i] >= grid[j][i] {
-					lower = false
-				}
-			}
-			if lower {
-				n += 1
-				continue
-			}
-			lower = true
-			for k := j + 1; k < len(grid) && lower; k++ {
-				if grid[k][i] >= grid[j][i] {
-					lower = false
-				}
-			}
-			if lower {
-				n += 1
-				continue
-			}
-		}
-	}
 	highest := 0
 	for i := 1; i < len(grid[0])-1; i++ {
 		for j := 1; j < len(grid)-1; j++ {
-			score, k := 1, 0
+			score, k, visible := 1, 0, false
 			for k = i - 1; k > 0 && grid[j][k] < grid[j][i]; k-- {
 			}
+			visible = visible || k == 0 && grid[j][k] < grid[j][i]
 			score *= (i - k)
 			for k = i + 1; k < len(grid[j])-1 && grid[j][k] < grid[j][i]; k++ {
 			}
+			visible = visible || k == len(grid[j])-1 && grid[j][k] < grid[j][i]
 			score *= (k - i)
 			for k = j - 1; k > 0 && grid[k][i] < grid[j][i]; k-- {
 			}
+			visible = visible || k == 0 && grid[k][i] < grid[j][i]
 			score *= (j - k)
 			for k = j + 1; k < len(grid)-1 && grid[k][i] < grid[j][i]; k++ {
 			}
+			visible = visible || k == len(grid)-1 && grid[k][i] < grid[j][i]
 			score *= (k - j)
 			if score > highest {
 				highest = score
+			}
+			if visible {
+				n += 1
 			}
 		}
 	}
