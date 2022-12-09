37a86b9860dc98943493dbff573fc3fb2697b616
pin2t <pin2t@mail.ru>
2022-12-09 11:46:57 +0300

    day 9 more oo

--- 9.swift
+++ 9.swift
@@ -1,5 +1,16 @@
 struct Pos: Hashable {
     var x = 0, y = 0
+
+    mutating func adjust(_ to: Pos) {
+        if abs(to.x - self.x) > 1 ||
+            abs(to.y - self.y) > 1 {
+            self.x += direction(self.x, to.x)
+            self.y += direction(self.y, to.y)
+        }
+    }
+    func direction(_ from: Int, _ to: Int) -> Int {
+        return from == to ? 0 : (from > to ? -1 : 1)
+    }
 }
 
 struct Rope {
@@ -14,17 +25,9 @@ struct Rope {
         knots[0].x += dx
         knots[0].y += dy
         for i in 1..<knots.count {
-            if abs(knots[i - 1].x - knots[i].x) > 1 ||
-               abs(knots[i - 1].y - knots[i].y) > 1 {
-                knots[i].x += direction(knots[i].x, knots[i - 1].x)
-                knots[i].y += direction(knots[i].y, knots[i - 1].y)
-            }
+            knots[i].adjust(knots[i - 1])
         }
         visited.insert(knots.last!)
-    }
-
-    func direction(_ from: Int, _ to: Int) -> Int {
-        return from == to ? 0 : (from > to ? -1 : 1)
     }
 }
 
