43a247ab1e99bbe12d882faa0feaeb3bf763ddd3
pin2t <pin2t@mail.ru>
2022-12-15 10:49:56 +0300

    day 15

--- 11.swift
+++ 11.swift
@@ -2,10 +2,13 @@ import Foundation
 
 extension String {
     var numbers: [Int] {
-        guard let regex = try? NSRegularExpression(pattern: "\\d+", options: [.caseInsensitive]) else { 
+        guard let regex = try? NSRegularExpression(
+            pattern: "\\d+", 
+            options: [.caseInsensitive]) else { 
             return [] 
         }
-        let matches  = regex.matches(in: self, options: [], range: NSMakeRange(0, self.count))
+        let matches  = regex.matches(in: self, options: [], 
+            range: NSMakeRange(0, self.count))
         return matches.map { match in
             String(self[Range(match.range, in: self)!])
         }.map { 
@@ -14,8 +17,96 @@ extension String {
     }
 }
 
-while let line = readLine() {
-    if line.hasPrefix("Monkey ") {
-        
+struct Monkey {
+    var items: [Int] = [], destinations: [Int] = []
+    var op: [String] = []
+    var inspected: Int = 0, div: Int = 1
+
+    mutating func inspect(_ ms: [Monkeys], _ relief: Bool) {
+        for item in self.items {
+            let lcm = ms.map { $0.div }.reduce(1, *)
+            var level: Int = 0, arg: Int = op[1] == "old" ? item : Int(op[1])!
+            switch op[0] {
+                case "+": level = item + arg
+                case "*": level = item * arg
+                default: break
+            }
+            if relief { level /= 3 }
+            level %= lcm
+            if level % self.div == 0 {
+                to = lines[3]!.numbers[0]
+            } else {
+                to = lines[4]!.numbers[0]
+            }
+
+        }
+        self.inspected += self.items.count
+        self.items = []
     }
 }
+
+var monkeys: [Monkey] = [], monkeys2: [Monkey] = []
+while let line = readLine() {
+    if line.hasPrefix("Monkey ") {
+        let lines = [readLine(), readLine(), readLine(), readLine(), readLine()]
+        print("items", lines[0]!.numbers, 
+            "formula", lines[1]!.components(separatedBy: " "),
+            "div", lines[2]!.numbers[0], "throw", lines[3]!.numbers[0], lines[4]!.numbers[0])
+        var monkey = Monkey() { item in 
+            let lcm = monkeys.map { $0.div }.reduce(1, *)
+            print("lcm", lcm)
+            let op: [String] = lines[1]!.components(separatedBy: " ").suffix(2)
+            var level: Int = 0, arg: Int = op[1] == "old" ? item : Int(op[1])!
+            switch op[0] {
+                case "+": level = (item + arg / 3) % lcm
+                case "*": level = (item * arg / 3) % lcm
+                default: break
+            }
+            var to: Int
+            if level % lines[2]!.numbers[0] == 0 {
+                to = lines[3]!.numbers[0]
+            } else {
+                to = lines[4]!.numbers[0]
+            }
+            monkeys[to].items.append(level)
+        }
+        monkey.div = lines[2]!.numbers[0]
+        monkey.items = lines[0]!.numbers
+        monkeys.append(monkey)
+        // monkey = Monkey() { item in 
+        //     let lcm = monkeys2.map { $0.div }.reduce(1, *)
+        //     print("lcm", lcm)
+        //     let op: [String] = lines[1]!.components(separatedBy: " ").suffix(2)
+        //     var level: Int = 0, arg: Int = op[1] == "old" ? item : Int(op[1])!
+        //     switch op[0] {
+        //         case "+": level = (item + arg) % lcm
+        //         case "*": level = (item * arg) % lcm
+        //         default: break
+        //     }
+        //     var to: Int
+        //     if level % lines[2]!.numbers[0] == 0 {
+        //         to = lines[3]!.numbers[0]
+        //     } else {
+        //         to = lines[4]!.numbers[0]
+        //     }
+        //     monkeys2[to].items.append(level)
+        // }
+        // monkey.div = lines[2]!.numbers[0]
+        // monkey.items = lines[0]!.numbers
+        // monkeys2.append(monkey)
+   }
+}
+for _ in 0..<20 {
+    for i in 0..<monkeys.count { 
+        monkeys[i].inspectAll() 
+    }
+}
+for _ in 0..<10000 {
+    for i in 0..<monkeys2.count { 
+        monkeys2[i].inspectAll() 
+    }
+}
+monkeys.sort { $1.inspected > $0.inspected }
+monkeys2.sort { $1.inspected > $0.inspected }
+print(monkeys.suffix(2)[0].inspected * monkeys.suffix(2)[1].inspected,
+      monkeys2.suffix(2)[0].inspected * monkeys2.suffix(2)[1].inspected)
---
+++ 15.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+type interval struct {
+	left, right int
+}
+
+func (i interval) intersect(with interval) bool {
+	return i.right >= with.left && i.left <= with.left ||
+		with.right >= i.left && with.left <= i.left
+}
+
+func (i interval) merge(with interval) interval {
+	if i.intersect(with) {
+		return interval{min(i.left, with.left), max(i.right, with.right)}
+	}
+	return i
+}
+
+func (i interval) len() int {
+	return i.right - i.left + 1
+}
+
+type pos struct{ x, y int }
+type sensor struct {
+	pos    pos
+	beacon pos
+}
+
+func (s sensor) merge(coverage []interval, targety int) []interval {
+	result := make([]interval, len(coverage))
+	copy(result, coverage)
+	distance := abs(s.beacon.x-s.pos.x) + abs(s.beacon.y-s.pos.y)
+	if abs(targety-s.pos.y) <= distance {
+		result = append(result, interval{
+			s.pos.x - (distance - abs(targety-s.pos.y)),
+			s.pos.x + (distance - abs(targety-s.pos.y)),
+		})
+		for {
+			merged := []interval{}
+			skip := map[int]bool{}
+			for i := 0; i < len(result); i++ {
+				if _, found := skip[i]; found {
+					continue
+				}
+				var intersect bool = false
+				for j := i + 1; j < len(result); j++ {
+					if result[i].intersect(result[j]) {
+						merged = append(merged, result[i].merge(result[j]))
+						skip[j] = true
+						intersect = true
+					}
+				}
+				if !intersect {
+					merged = append(merged, result[i])
+				}
+			}
+			if len(merged) == len(result) {
+				break
+			}
+			result = merged
+		}
+	}
+	return result
+}
+
+func beacons(sensors []sensor, targety int) int {
+	result := map[int]bool{}
+	for _, b := range sensors {
+		if b.beacon.y == targety {
+			result[b.beacon.x] = true
+		}
+	}
+	return len(result)
+}
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	sensors := []sensor{}
+	for input.Scan() {
+		var s sensor
+		fmt.Sscanf(input.Text(), "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d",
+			&s.pos.x, &s.pos.y, &s.beacon.x, &s.beacon.y)
+		sensors = append(sensors, s)
+	}
+	coverage := []interval{}
+	for _, s := range sensors {
+		coverage = s.merge(coverage, 2000000)
+	}
+	n1 := 0
+	for _, c := range coverage {
+		n1 += c.len()
+	}
+	n1 -= beacons(sensors, 2000000)
+	for y := 0; y <= 4000000; y++ {
+		coverage := []interval{}
+		for _, s := range sensors {
+			coverage = s.merge(coverage, y)
+		}
+		if len(coverage) > 1 {
+			fmt.Println(n1, (coverage[0].right+1)*4000000+y)
+			break
+		}
+	}
+}
