4f751fd365cd004c0f873a3d8544f23de02a4d9a
pin2t <pin2t@mail.ru>
2022-12-20 14:58:33 +0300

    day 16 part 2

--- 16.go
+++ 16.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"regexp"
 	"strconv"
@@ -15,25 +16,25 @@ func max(a, b int) int {
 	return b
 }
 
-type sset map[string]bool
+type vset map[int]bool
 
-func (set sset) contains(key string) bool {
+func (set vset) contains(key int) bool {
 	_, result := set[key]
 	return result
 }
 
-var flow = map[string]int{}
-var tunnels = sset{}
+var flow = map[int]int{}
+var tunnels = vset{}
 
 type state struct {
-	valve    string
-	opened   sset
+	valve    int
+	opened   vset
 	timeLeft int
 	pressure int
 }
 
 func (s state) open() state {
-	opened := sset(make(map[string]bool, 50))
+	opened := vset{}
 	for k, v := range s.opened {
 		opened[k] = v
 	}
@@ -46,62 +47,67 @@ func (s state) open() state {
 	}
 }
 
-func (s state) step(to string) state {
+func (s state) step(to int) state {
 	return state{to, s.opened, s.timeLeft - 1, s.pressure}
 }
 
-type state2 struct {
-	valve, evalve string
-	opened        sset
-	timeLeft      int
-	pressure      int
-}
-
-func (s state2) open() state2 {
-	opened := sset(make(map[string]bool, 50))
-	for k, v := range s.opened {
-		opened[k] = v
-	}
-	pressure := s.pressure
-	if flow[s.valve] > 0 {
-		opened[s.valve] = true
-		pressure += flow[s.valve] * (s.timeLeft - 1)
-	}
-	if flow[s.evalve] > 0 {
-		opened[s.evalve] = true
-		pressure += flow[s.evalve] * (s.timeLeft - 1)
-	}
-	return state2{s.valve, s.evalve, opened, s.timeLeft - 1, pressure}
-}
-
-func (s state2) step(to, eto string) state2 {
-	return state2{to, eto, s.opened, s.timeLeft - 1, s.pressure}
-}
-
-func pressure(initial state, forbidden sset) int {
+func pressure(initial state, forbidden []int) int {
 	result := 0
-	processed := sset(make(map[string]bool, 10000000))
-	queue := make([]state, 10000000)
+	processed := vset{}
+	queue := []state{}
 	queue = append(queue, initial)
 	for len(queue) > 0 {
 		s := queue[0]
 		queue = queue[1:]
-		key := fmt.Sprintf("%s,%d", s.valve, s.pressure)
-		if processed.contains(key) || s.timeLeft == 0 {
+		if processed.contains(s.pressure*26*26+s.valve) || s.timeLeft == 0 {
 			continue
 		}
-		processed[key] = true
+		processed[s.pressure*26*26+s.valve] = true
 		result = max(result, s.pressure)
-		if !s.opened.contains(s.valve) && !forbidden.contains(s.valve) && flow[s.valve] > 0 {
+		if !s.opened.contains(s.valve) && !contains(forbidden, s.valve) && flow[s.valve] > 0 {
 			queue = append(queue, s.open())
 		}
 		for to, _ := range flow {
-			if to != s.valve && !forbidden.contains(to) && tunnels.contains(s.valve+to) {
+			if to != s.valve && !contains(forbidden, to) && tunnels.contains(s.valve*26*26+to) {
 				queue = append(queue, s.step(to))
 			}
 		}
 	}
 	return result
+}
+
+func key(valve string) int {
+	return int(valve[0]-'A')*26 + int(valve[1]-'A')
+}
+
+func key2(v1, v2 string) int {
+	return key(v1)*26*26 + key(v2)
+}
+
+func powerset(original []int) [][]int {
+	size := int(math.Pow(2, float64(len(original))))
+	result := [][]int{}
+	var index int
+	for index < size {
+		var subSet []int
+		for j, elem := range original {
+			if index&(1<<uint(j)) > 0 {
+				subSet = append(subSet, elem)
+			}
+		}
+		result = append(result, subSet)
+		index++
+	}
+	return result
+}
+
+func contains(arr []int, key int) bool {
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == key {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
@@ -111,22 +117,29 @@ func main() {
 	for scanner.Scan() {
 		names := reNames.FindAllString(scanner.Text(), -1)
 		f, _ := strconv.ParseInt(reFlow.FindString(scanner.Text()), 0, 0)
-		flow[names[0]] = int(f)
+		flow[key(names[0])] = int(f)
 		for i := 1; i < len(names); i++ {
-			tunnels[names[0]+names[i]] = true
+			tunnels[key2(names[0], names[i])] = true
 		}
 	}
-	n1 := pressure(state{"AA", sset{}, 30, 0}, sset{})
-	first := sset{}
-	second := sset{}
-	for v, _ := range flow {
-		second[v] = true
+	valves := []int{}
+	nonempty := []int{}
+	for v, f := range flow {
+		valves = append(valves, v)
+		if f != 0 {
+			nonempty = append(nonempty, v)
+		}
 	}
+	n1 := pressure(state{0, vset{}, 30, 0}, []int{})
 	n2 := 0
-	for v, _ := range second {
-		n2 = max(n2, pressure(state{"AA", sset{}, 26, 0}, first)+pressure(state{"AA", sset{}, 26, 0}, second))
-		delete(second, v)
-		first[v] = true
+	for _, first := range powerset(nonempty) {
+		second := []int{}
+		for v, f := range flow {
+			if !contains(first, v) && f > 0 {
+				second = append(second, v)
+			}
+		}
+		n2 = max(n2, pressure(state{0, vset{}, 26, 0}, first)+pressure(state{0, vset{}, 26, 0}, second))
 	}
 	fmt.Println(n1, n2)
 }
