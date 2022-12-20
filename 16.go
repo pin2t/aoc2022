f3658b81bc304525e4880d09c30138f1975975f3
pin2t <pin2t@mail.ru>
2022-12-20 15:30:45 +0300

    day 16 still slow

--- 16.go
+++ 16.go
@@ -51,7 +51,7 @@ func (s state) step(to int) state {
 	return state{to, s.opened, s.timeLeft - 1, s.pressure}
 }
 
-func pressure(initial state, forbidden []int) int {
+func pressure(initial state, forbidden vset) int {
 	result := 0
 	processed := vset{}
 	queue := []state{}
@@ -59,16 +59,17 @@ func pressure(initial state, forbidden []int) int {
 	for len(queue) > 0 {
 		s := queue[0]
 		queue = queue[1:]
-		if processed.contains(s.pressure*26*26+s.valve) || s.timeLeft == 0 {
+		key := s.pressure*26*26 + s.valve
+		if processed.contains(key) || s.timeLeft == 0 {
 			continue
 		}
-		processed[s.pressure*26*26+s.valve] = true
+		processed[key] = true
 		result = max(result, s.pressure)
-		if !s.opened.contains(s.valve) && !contains(forbidden, s.valve) && flow[s.valve] > 0 {
+		if !s.opened.contains(s.valve) && !forbidden.contains(s.valve) && flow[s.valve] > 0 {
 			queue = append(queue, s.open())
 		}
 		for to, _ := range flow {
-			if to != s.valve && !contains(forbidden, to) && tunnels.contains(s.valve*26*26+to) {
+			if to != s.valve && !forbidden.contains(to) && tunnels.contains(s.valve*26*26+to) {
 				queue = append(queue, s.step(to))
 			}
 		}
@@ -84,30 +85,21 @@ func key2(v1, v2 string) int {
 	return key(v1)*26*26 + key(v2)
 }
 
-func powerset(original []int) [][]int {
-	size := int(math.Pow(2, float64(len(original))))
-	result := [][]int{}
+func powerset(set []int) []vset {
+	size := int(math.Pow(2, float64(len(set))))
+	result := []vset{}
 	var index int
 	for index < size {
-		var subSet []int
-		for j, elem := range original {
+		subSet := vset{}
+		for j, elem := range set {
 			if index&(1<<uint(j)) > 0 {
-				subSet = append(subSet, elem)
+				subSet[elem] = true
 			}
 		}
 		result = append(result, subSet)
 		index++
 	}
 	return result
-}
-
-func contains(arr []int, key int) bool {
-	for i := 0; i < len(arr); i++ {
-		if arr[i] == key {
-			return true
-		}
-	}
-	return false
 }
 
 func main() {
@@ -130,13 +122,13 @@ func main() {
 			nonempty = append(nonempty, v)
 		}
 	}
-	n1 := pressure(state{0, vset{}, 30, 0}, []int{})
+	n1 := pressure(state{0, vset{}, 30, 0}, vset{})
 	n2 := 0
-	for _, first := range powerset(nonempty) {
-		second := []int{}
+	for nset, first := range powerset(nonempty) {
+		second := vset{}
 		for v, f := range flow {
-			if !contains(first, v) && f > 0 {
-				second = append(second, v)
+			if !first.contains(v) && f > 0 {
+				second[v] = true
 			}
 		}
 		n2 = max(n2, pressure(state{0, vset{}, 26, 0}, first)+pressure(state{0, vset{}, 26, 0}, second))
