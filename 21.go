32a4bfc302f93dd5bd0b4ca7e849b58a4699385f
pin2t <pin2t@mail.ru>
2022-12-22 06:31:27 +0300

    simplify

--- 21.go
+++ 21.go
@@ -13,11 +13,13 @@ type monkey struct {
 	op   string
 }
 
-func (m monkey) yell(monkeys map[string]monkey) float64 {
+var monkeys = map[string]monkey{}
+
+func (m monkey) yell() float64 {
 	if m.n > 0 {
 		return m.n
 	}
-	yell1, yell2 := monkeys[m.wait[0]].yell(monkeys), monkeys[m.wait[1]].yell(monkeys)
+	yell1, yell2 := monkeys[m.wait[0]].yell(), monkeys[m.wait[1]].yell()
 	switch m.op {
 	case "+":
 		return yell1 + yell2
@@ -31,13 +33,8 @@ func (m monkey) yell(monkeys map[string]monkey) float64 {
 	panic("unknown operation" + fmt.Sprintf("%v", m))
 }
 
-func (m monkey) compare(monkeys map[string]monkey) float64 {
-	return monkeys[m.wait[0]].yell(monkeys) - monkeys[m.wait[1]].yell(monkeys)
-}
-
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	monkeys := map[string]monkey{}
 	for scanner.Scan() {
 		var name string
 		var n int64
@@ -49,18 +46,20 @@ func main() {
 		}
 		monkeys[name[:4]] = monkey{float64(n), []string{w1, w2}, op}
 	}
-	n1 := monkeys["root"].yell(monkeys)
+	n1 := monkeys["root"].yell()
+
 	low, high := int64(1), int64(1000000000000000)
 	monkeys["humn"] = monkey{float64(low + (high-low)/2), []string{}, ""}
 	root := monkeys["root"]
+	root.op = "-"
 	for {
-		cmp := root.compare(monkeys)
+		cmp := root.yell()
 		if cmp > 0 {
 			low += (high - low) / 2
 		} else if cmp < 0 {
 			high -= (high - low) / 2
 		} else if cmp == 0 {
-			fmt.Println(int64(n1), int64(monkeys["humn"].n))
+			fmt.Println(int64(n1), int64(monkeys["humn"].yell()))
 			return
 		}
 		monkeys["humn"] = monkey{float64(low + (high-low)/2), []string{}, ""}
