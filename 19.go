d073a6ae082a13632bcce6c6edfe073daab1ef2d
pin2t <pin2t@mail.ru>
2022-12-25 17:19:04 +0300

    fix

--- 19.go
+++ 19.go
@@ -4,14 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
-
-type blueprint struct {
-	n         int
-	ore, clay int
-	obsidian  struct{ ore, clay int }
-	geode     struct{ ore, obsidian int }
-}
 
 func main() {
 	blueprints := []blueprint{}
@@ -23,5 +17,102 @@ func main() {
 			&b.n, &b.ore, &b.clay, &b.obsidian.ore, &b.obsidian.clay, &b.geode.ore, &b.geode.obsidian)
 		blueprints = append(blueprints, b)
 	}
-	fmt.Println(blueprints)
+	sum := 0
+	for _, bp := range blueprints {
+		sum += bp.n * maxgeodes(bp, 24, 5000000)
+	}
+	prod := 1
+	for _, bp := range blueprints[:3] {
+		prod *= maxgeodes(bp, 32, 5000000)
+	}
+	fmt.Println(sum, prod)
+}
+
+type blueprint struct {
+	n         int
+	ore, clay int
+	obsidian  struct{ ore, clay int }
+	geode     struct{ ore, obsidian int }
+}
+
+type items struct{ ore, clay, obsidian, geode int }
+
+func (i items) add(ore, clay, obsidian, geode int) items {
+	return items{i.ore + ore, i.clay + clay, i.obsidian + obsidian, i.geode + geode}
+}
+
+type state struct {
+	minute int
+	values items
+	robots items
+}
+
+func (s state) val() int {
+	return s.robots.ore + s.robots.clay*10 + s.robots.obsidian*100 + s.robots.geode*1000
+}
+
+func maxgeodes(bp blueprint, minutes, prune int) int {
+	max := 0
+	maxmin := 0
+	processed := make(map[state]bool)
+	queue := []state{{0, items{0, 0, 0, 0}, items{1, 0, 0, 0}}}
+	for len(queue) > 0 {
+		if queue[0].minute > maxmin {
+			maxmin = queue[0].minute
+			processed = map[state]bool{}
+			if len(queue) > prune {
+				sort.Slice(queue, func(i, j int) bool {
+					return queue[i].val() > queue[j].val()
+				})
+				queue = queue[0:prune]
+				continue
+			}
+		}
+		s := queue[0]
+		queue = queue[1:]
+		if s.minute == minutes {
+			if s.values.geode > max {
+				max = s.values.geode
+			}
+			continue
+		}
+		if processed[s] {
+			continue
+		}
+		processed[s] = true
+		if s.values.ore >= bp.geode.ore && s.values.obsidian >= bp.geode.obsidian {
+			queue = append(queue, state{
+				s.minute + 1,
+				s.values.add(s.robots.ore-bp.geode.ore, s.robots.clay, s.robots.obsidian-bp.geode.obsidian, s.robots.geode),
+				s.robots.add(0, 0, 0, 1),
+			})
+		}
+		if s.values.ore >= bp.obsidian.ore && s.values.clay >= bp.obsidian.clay {
+			queue = append(queue, state{
+				s.minute + 1,
+				s.values.add(s.robots.ore-bp.obsidian.ore, s.robots.clay-bp.obsidian.clay, s.robots.obsidian, s.robots.geode),
+				s.robots.add(0, 0, 1, 0),
+			})
+		}
+		if s.values.ore >= bp.clay {
+			queue = append(queue, state{
+				s.minute + 1,
+				s.values.add(s.robots.ore-bp.clay, s.robots.clay, s.robots.obsidian, s.robots.geode),
+				s.robots.add(0, 1, 0, 0),
+			})
+		}
+		if s.values.ore >= bp.ore {
+			queue = append(queue, state{
+				s.minute + 1,
+				s.values.add(s.robots.ore-bp.ore, s.robots.clay, s.robots.obsidian, s.robots.geode),
+				s.robots.add(1, 0, 0, 0),
+			})
+		}
+		queue = append(queue, state{
+			s.minute + 1,
+			s.values.add(s.robots.ore, s.robots.clay, s.robots.obsidian, s.robots.geode),
+			s.robots,
+		})
+	}
+	return max
 }
