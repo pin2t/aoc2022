0d3bd92762eac5ffe940f4297993f967c881bf21
pin2t <pin2t@mail.ru>
2023-07-16 14:45:41 +0300

    fix

--- 24.go
+++ 24.go
@@ -30,13 +30,12 @@ func (p pos) move(delta pos) pos {
 }
 
 type state struct {
-	pos     pos
-	minutes int
-	stage   int // 0 - down, 1 - up, 2 - down again
+	pos         pos
+	time, stage int // 0 - down, 1 - up, 2 - down again
 }
 
-func (s state) next(to pos) state {
-	return state{to, s.minutes + 1, s.stage}
+func (s state) move(to pos) state {
+	return state{to, s.time + 1, s.stage}
 }
 
 func main() {
@@ -44,74 +43,76 @@ func main() {
 	grid := map[pos]rune{}
 	blizzards := map[pos]pos{}
 	bottomright := pos{0, 0}
+	directions := map[rune]pos{'<': {0, -1}, '>': {0, 1}, '^': {-1, 0}, 'v': {1, 0}}
 	for x := 0; scanner.Scan(); x++ {
 		for y, c := range scanner.Text() {
 			grid[pos{x, y}] = c
 			bottomright = bottomright.max(pos{x, y})
-			switch c {
-			case '<':
-				blizzards[pos{x, y}] = pos{-1, 0}
-			case '>':
-				blizzards[pos{x, y}] = pos{1, 0}
-			case '^':
-				blizzards[pos{x, y}] = pos{0, -1}
-			case 'v':
-				blizzards[pos{x, y}] = pos{0, 1}
+			if c != '#' && c != '.' {
+				blizzards[pos{x, y}] = directions[c]
 			}
 		}
 	}
-	deltas := []pos{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
-	queue := []state{{pos{0, 1}, 0, 0}}
-	processed := map[state]bool{}
-	n1 := 0
+	queue := make([]state, 1000000)
+	queue = append(queue, state{pos{0, 1}, 0, 0})
+	processed := make(map[state]bool, 1000)
+	n1, maxtime, step := 0, 0, 0
 	for {
-		st := queue[0]
+		step++
+		s := queue[0]
 		queue = queue[1:]
-		if processed[st] {
+		if s.time > maxtime {
+			processed = make(map[state]bool, 1000)
+			maxtime = s.time
+		}
+		if processed[s] {
 			continue
 		}
-		processed[st] = true
+		processed[s] = true
 		stopped := false
 		for sp, d := range blizzards {
-			if sp.x == st.pos.x {
-				sy := mod((sp.y+(d.y*st.minutes)-1), bottomright.y-1) + 1
-				if sy == st.pos.y {
+			if sp.x == s.pos.x && d.y != 0 {
+				sy := mod(sp.y+(d.y*s.time)-1, bottomright.y-1) + 1
+				if sy == s.pos.y {
 					stopped = true
+					break
 				}
-			}
-			if sp.y == st.pos.y {
-				sx := mod((sp.x+(d.x*st.minutes)-1), bottomright.x-1) + 1
-				if sx == st.pos.x {
+			} else if sp.y == s.pos.y && d.x != 0 {
+				sx := mod(sp.x+(d.x*s.time)-1, bottomright.x-1) + 1
+				if sx == s.pos.x {
 					stopped = true
+					break
 				}
 			}
 		}
 		if stopped {
 			continue
 		}
-		if st.pos.x == 0 && st.stage > 1 {
-			st.stage++
+		if s.pos.x == 0 && s.stage == 1 {
+			s.stage++
 		}
-		if st.pos.x == bottomright.x {
-			if st.stage == 0 {
-				n1 = st.minutes
-				st.stage++
+		if s.pos.x == bottomright.x {
+			if s.stage == 0 {
+				if n1 == 0 {
+					n1 = s.time
+				}
+				s.stage = 1
 			}
-			if st.stage > 1 {
-				fmt.Println(n1, st.minutes)
-				break
+			if s.stage > 1 {
+				fmt.Println(n1, s.time)
+				return
 			}
 		}
-		for _, d := range deltas {
-			to := st.pos.move(pos{d.x, d.y})
+		for _, d := range directions {
+			to := s.pos.move(d)
 			if grid[to] == '#' {
 				continue
 			}
 			if _, ok := grid[to]; !ok {
 				continue
 			}
-			queue = append(queue, st.next(to))
+			queue = append(queue, s.move(to))
 		}
-		queue = append(queue, st.next(st.pos))
+		queue = append(queue, s.move(s.pos))
 	}
 }
