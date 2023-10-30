6e71361b2b8b0b96a462e084890e525782a1efc8
pin2t <pin2t@mail.ru>
2023-10-30 12:27:39 +0500

    keep up swift

--- 23.swift
+++ 23.swift
@@ -1,80 +1,65 @@
 import Foundation
 
-struct Position: Hashable {
+struct Pos: Hashable {
     var x, y: Int
 
     init(_ x: Int, _ y: Int) { self.x = x; self.y = y; }
 }
 
-var elves = Set<Position>()
+var elves = Set<Pos>()
 var y = 0
 while let line = readLine() {
     for (x, c) in line.enumerated() {
-        if c == "#" { elves.insert(Position(x, y)) }
+        if c == "#" { elves.insert(Pos(x, y)) }
     }
     y += 1
 }
 var (n1, n2, i) = (0, 0, 0)
 while true {
-    var moves = [Position: Position]()
+    var propositions = [Pos: Pos]()
     for e in elves {
-        var north = false, south = false, west = false, east = false
-        for d in [Position(0, -1), Position(0, 1), Position(-1, 0), Position(1, 0)] {
-            if elves.contains(Position(e.x + d.x, e.y + d.y)) {
-                if d.y == -1 { north = true }
-                if d.y == 1 { south = true }
-                if d.x == -1 { west = true }
-                if d.x == 1 { east = true }
-            }
-        }
-        if !north && !south && !west && !east {
-            continue
-        }
-        let can = [(can: north, pos: Position(e.x, e.y - 1)),
-                   (can: south, pos: Position(e.x, e.y + 1)),
-                   (can: west, pos: Position(e.x - 1, e.y)),
-                   (can: east, pos: Position(e.x + 1, e.y))]
-        for j in 0..<4 {
-            let to = (i + j) % 4
-            if can[to].can {
-                moves[e] = can[to].pos
-                break
+        if elves.contains(where :{ $0 == Pos(e.x, e.y - 1) || $0 == Pos(e.x, e.y + 1) ||
+                            $0 == Pos(e.x - 1, e.y) || $0 == Pos(e.x + 1, e.y) ||
+                            $0 == Pos(e.x - 1, e.y - 1) || $0 == Pos(e.x + 1, e.y + 1) ||
+                            $0 == Pos(e.x - 1, e.y + 1) || $0 == Pos(e.x + 1, e.y - 1) }) {
+            let moves = [
+                [Pos(e.x, e.y - 1), Pos(e.x, e.y - 1), Pos(e.x - 1, e.y - 1), Pos(e.x + 1, e.y - 1)],
+    			[Pos(e.x, e.y + 1), Pos(e.x, e.y + 1), Pos(e.x - 1, e.y + 1), Pos(e.x + 1, e.y + 1)],
+                [Pos(e.x - 1, e.y), Pos(e.x - 1, e.y), Pos(e.x - 1, e.y - 1), Pos(e.x - 1, e.y + 1)],
+                [Pos(e.x + 1, e.y), Pos(e.x + 1, e.y), Pos(e.x + 1, e.y - 1), Pos(e.x + 1, e.y + 1)]
+            ]
+            for j in 0..<4 {
+                let to = (i + j) % 4
+                if !elves.contains(moves[to][1]) && !elves.contains(moves[to][2]) && !elves.contains(moves[to][3]) {
+                    propositions[e] = moves[to][0]
+                    break
+                }
             }
         }
     }
-    var wills = [Position: Int]()
-    for (_, to) in moves {
-        wills[to, default: 0] += 1
+    var npropositions = [Pos:Int]()
+    for (from, to) in propositions {
+        npropositions[to] = (npropositions[to] ?? 0) + 1
     }
-    var moved = Set<Position>()
-    for (p, to) in moves {
-        if wills[to] == 1 {
-            moved.insert(to)
-            elves.remove(p)
+    var moved = 0
+    for (from, to) in propositions {
+        if npropositions[to] == 1 {
+            elves.remove(from)
+            elves.insert(to)
+            moved += 1
         }
     }
-    if moved.isEmpty {
-        n2 = i
+    if moved == 0 {
+        n2 = i + 1
         break
     }
-    for e in elves {
-        moved.insert(e)
-    }
-    elves = moved
     if i == 9 {
-        var top = 1000000, left = 1000000
-        var bottom = -1000000, right = -1000000
+        var top = 1000000, left = 1000000, bottom = -1000000, right = -1000000
         for e in elves {
-            left = min(left, e.x);   top = min(top, e.y)
-            right = max(right, e.x); bottom = max(bottom, e.y)
+            (left, right, top, bottom) = (min(left, e.x), max(right, e.x), min(top, e.y), max(bottom, e.y))
         }
-        var field = 0
-        for x in left...right {
-            for y in top...bottom {
-                if elves.contains(Position(x, y)) { field += 1 }
-            }
-        }
-        n1 = field
+        n1 = (bottom - top + 1) * (right - left + 1) - elves.count
     }
+    i += 1
 }
 print(n1, n2)
